9f16edb780a853f4c989f1f18c6e36799bd68f79
Matthis323 <79197590+Matthis323@users.noreply.github.com>
2026-02-04 10:21:41 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -79,4 +79,4 @@ def readyz():
     return jsonify({"status": "ready"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
